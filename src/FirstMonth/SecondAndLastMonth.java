59dfd1e5cda7157887ca9b161202b9697270423d
nabiullahbenish <nabiullah_benash@yahoo.com>
2024-08-13 15:18:05 -0700

    I have posted the first step of new project.

---
+++ src/FirstMonth/SecondAndLastMonth.java
@@ -0,0 +1,16 @@
+package FirstMonth;
+
+public class SecondAndLastMonth {
+
+
+    public static void main(String [] args) {
+
+
+        System.out.println("Welcome to the Untied State of America");
+    }
+
+
+
+
+
+}
